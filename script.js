846af9f309b41a700717dc5db677bd1e715f4792
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-02-25 14:27:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
-  // your code here
+ // Remove leading and trailing spaces
+    s = s.trim();
+    if (!s) {
+        return s;  // Return empty string if input is empty
+    } else {
+        // Split the string by space and return the first word
+        return s.split(' ')[0];
+    }
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)); 
